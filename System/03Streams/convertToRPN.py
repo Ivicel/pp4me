508be8724f79ccc0d1dacdcfd41031dd539de80e
Sedny <SednyLeung@gmail.com>
2015-08-06 00:15:00 +0800

    modify convertToRPN

--- System/03Streams/convertToRPN.py
+++ System/03Streams/convertToRPN.py
@@ -12,7 +12,7 @@ def convert(express):
 	stack = []
 	for n in exp:
 		if n.isdigit():
-			result.appen(n)
+			result.append(' ' + n + ' ')
 		elif n == ' ':
 			continue
 		elif n in ['+', '-', ')']:
@@ -20,12 +20,23 @@ def convert(express):
 				t = ''.join(stack).rsplit('(', 1)
 				stack = list(t[0])
 				result.append(t[1][-1::-1])
+			else:
+				l = stack[-1::-1]
+				#result += [stack.pop() for m in l[:l.index('(')]]
+				for k in stack[-1::-1]:
+					if k != '(':
+						result.append(stack.pop())
+					else:
+						break
+				stack.append(n)
 		elif n in ['/', '*', '(']:
-
+			if stack[-1:] in ['/', '*']:
+				result.append(stack.pop())
+			stack.append(n)
 		else:
-			raise ValueError 'Only can input "+-*/()"'
+			raise ValueError('Only can input "+-*/()"')
 	else:
-		return ''.join(result)
+		return ''.join(result + stack[-1::-1])
 
 if __name__ == '__main__':
 	result = convert(sys.argv[1])
